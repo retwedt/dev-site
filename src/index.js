169d690e0ff57fb178f39e329743060770c3ab26
rex <retwedt@gmail.com>
2019-06-06 23:58:57 -0500

    Dropdown menu on mobile.

--- src/index.js
+++ src/index.js
@@ -5,5 +5,31 @@
  */
 
 require('./styles/main.styl')
+require('./img/nav-logo.png')
 
-console.log("hello world!@@")
+console.log('hello world!@@')
+
+const navToggle = document.querySelector('.nav-toggle')
+console.log(navToggle)
+const navCollapse = document.querySelector('.nav-collapse')
+console.log(navCollapse)
+
+//*******************onload event*******************
+// when window loads, setup events on buttons and browser
+window.onload = () => {
+  navToggle.addEventListener('click', () => {
+    console.log('You clicked it!')
+    const open = navCollapse.classList.contains('open')
+    if (open) {
+      navToggle.classList.remove('open')
+      navToggle.classList.add('closed')
+      navCollapse.classList.remove('open')
+      navCollapse.classList.add('closed')
+    } else {
+      navToggle.classList.remove('closed')
+      navToggle.classList.add('open')
+      navCollapse.classList.remove('closed')
+      navCollapse.classList.add('open')
+    }
+  })
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,121 +1,146 @@
 //main.js
 // ajax content into #content div for a seamless user experience
 
-
 //*******************html content*******************
-var homeURL = "content/home.html";
+var homeURL = 'content/home.html'
 
 //*******************get dom elements*******************
 //get all links from nav wrapper and store them in an array
-var navDiv = document.querySelector(".nav");
-var lst = navDiv.querySelectorAll("a");
-//main content div
-var contentDiv = document.getElementById("content");
-//nav-panel-mobile for collapsing nav onclick
-var navPanelMobile = document.getElementById("nav-panel-mobile");
+var navDiv = document.querySelector('.nav')
 
+var lst = navDiv.querySelectorAll('a')
+//main content div
+var contentDiv = document.querySelector('content')
+//nav-panel-mobile for collapsing nav onclick
+var navPanelMobile = document.querySelector('nav-panel-mobile')
+
+const navToggle = document.querySelector('.nav-toggle')
+console.log(navToggle)
 
 //*******************onload event*******************
 // when window loads, setup events on buttons and browser
-window.onload = function () {
-	if (supports_history_api()) {
-		//parse url and load page once at the beginning
-		//used incase the visitor is coming from an external site (or bookmark)
-		var url = parseURL();
-		loadPage(url, false);
+window.onload = () => {
+  if (supports_history_api()) {
+    //parse url and load page once at the beginning
+    //used incase the visitor is coming from an external site (or bookmark)
+    var url = parseURL()
+    loadPage(url, false)
 
-		//add click events to all buttons in the nav-wrapper
-		for (var i = 0; i < lst.length; i++) {
-			addClicker(lst[i]);
-		}
+    //add click events to all buttons in the nav-wrapper
+    for (var i = 0; i < lst.length; i++) {
+      addClicker(lst[i])
+    }
 
-		window.addEventListener("popstate", function (e) {
-			var url = parseURL();
-			loadPage(url, false);
-		}, false);
-	}
+    window.addEventListener(
+      'popstate',
+      function(e) {
+        var url = parseURL()
+        loadPage(url, false)
+      },
+      false
+    )
+  }
+
+  navToggle.addEventListener('onclick', () => {
+    console.log('You clicked it!')
+  })
 }
 
-
-// check if browser supports history api
+/**
+ * Check if browser supports history api
+ */
 function supports_history_api() {
-	if (window.history) {
-		if (history.pushState) {
-			return true;
-		}
-	} else {
-		console.log("history api not supported!");
-		return false;
-	}
+  if (window.history && history.pushState) {
+    return true
+  } else {
+    console.log('history api not supported!')
+    return false
+  }
 }
-
 
 // load page function
 function loadPage(url, addToHistory) {
-	if (url != null) {
-		//send request
-		var requestReturn = "";
-		var myRequest = new XMLHttpRequest();
-		myRequest.open("GET", url, true);
-		myRequest.onreadystatechange = function () {
-			//process request function
-			if (myRequest.readyState === 4 && myRequest.status === 200) {
-				contentDiv.innerHTML = myRequest.responseText;
-			}
-		}
-		myRequest.send();
-	}
+  if (url != null) {
+    //send request
+    var requestReturn = ''
+    var myRequest = new XMLHttpRequest()
+    myRequest.open('GET', url, true)
+    myRequest.onreadystatechange = function() {
+      //process request function
+      if (myRequest.readyState === 4 && myRequest.status === 200) {
+        contentDiv.innerHTML = myRequest.responseText
+      }
+    }
+    myRequest.send()
+  }
 }
-
 
 //get hash from current url, construct custom url for ajax call
 function parseURL() {
-	var newHash = window.location.hash;
-	//if there is no hash (you are trying to go to the homepage)
-	if (!newHash) {
-		newHash = "#home";
-	}
-	newHash = newHash.slice(1);
-	var newURL = "content/" + newHash + ".html";
-	return newURL;
+  var newHash = window.location.hash
+  //if there is no hash (you are trying to go to the homepage)
+  if (!newHash) {
+    newHash = '#home'
+  }
+  newHash = newHash.slice(1)
+  var newURL = 'content/' + newHash + '.html'
+  return newURL
 }
-
 
 // add event listener to html element to load page when clicked on
 function addClicker(element) {
-	element.addEventListener("click", function (e) {
-		//prevend default action of button
-		e.preventDefault();
-		//check for href of element that was clicked on
-		var pushURL = element.href;
-		var index = pushURL.indexOf("#");
-		//if there is no href, the user is trying to go home
-		if (index < 0) {
-			pushURL = "home";
-			// update history differently on local vs. server
-			if (window.location.hostname.charAt(0) === "1") {
-				var fullURL = window.location.protocol + "//" + window.location.hostname + ":8080/"; //for node testing
-			} else {
-				var fullURL = window.location.protocol + "//" + window.location.hostname + "/"; //for server
-			}
-		} else {
-			pushURL = pushURL.slice(index + 1);
-			// update history differently on local vs. server
-			if (window.location.hostname.charAt(0) === "1") {
-				var fullURL = window.location.protocol + "//" + window.location.hostname + ":8080/#" + pushURL; //for node testing
-			} else {
-				var fullURL = window.location.protocol + "//" + window.location.hostname + "/#" + pushURL; //for server
-			}
-		}
+  element.addEventListener(
+    'click',
+    function(e) {
+      //prevend default action of button
+      e.preventDefault()
+      //check for href of element that was clicked on
+      var pushURL = element.href
+      var index = pushURL.indexOf('#')
+      //if there is no href, the user is trying to go home
+      if (index < 0) {
+        pushURL = 'home'
+        // update history differently on local vs. server
+        if (window.location.hostname.charAt(0) === '1') {
+          var fullURL =
+            window.location.protocol +
+            '//' +
+            window.location.hostname +
+            ':8080/' //for node testing
+        } else {
+          var fullURL =
+            window.location.protocol + '//' + window.location.hostname + '/' //for server
+        }
+      } else {
+        pushURL = pushURL.slice(index + 1)
+        // update history differently on local vs. server
+        if (window.location.hostname.charAt(0) === '1') {
+          var fullURL =
+            window.location.protocol +
+            '//' +
+            window.location.hostname +
+            ':8080/#' +
+            pushURL //for node testing
+        } else {
+          var fullURL =
+            window.location.protocol +
+            '//' +
+            window.location.hostname +
+            '/#' +
+            pushURL //for server
+        }
+      }
 
-		//if user clicks on projects or gallery button or mobile menu button, cancel the ajax call, you don't want to load these pages!
-		if (pushURL == "projects") {
-			return false;
-		}
+      //if user clicks on projects or gallery button or mobile menu button, cancel the ajax call, you don't want to load these pages!
+      if (pushURL == 'projects') {
+        return false
+      }
 
-		//create url for ajax call
-		pushURL = "content/" + pushURL + ".html";
-		loadPage(pushURL, true);
-		history.pushState(" ", null, fullURL);
-	}, true);
+      //create url for ajax call
+      pushURL = 'content/' + pushURL + '.html'
+      loadPage(pushURL, true)
+      history.pushState(' ', null, fullURL)
+    },
+    true
+  )
 }
