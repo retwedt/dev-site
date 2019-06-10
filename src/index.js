744f17ee981b3b1f048dc565b210a96a243fbc44
rex <retwedt@gmail.com>
2019-06-09 23:23:29 -0500

    Move nav button config to the index.js page.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
  */
 
 import { toggleNavMenu, initNavMenuResizeListener } from "./js/menu";
+import { supports_history_api, loadPage, parseURL } from "./js/main";
 
 import "./styles/main.styl";
 
@@ -14,6 +15,54 @@ const copyrightYearEl = document.querySelector("#copyright-year");
 
 // When window loads, setup events on buttons and browser
 window.onload = () => {
+  if (supports_history_api()) {
+    // Parse url and load page
+    const url = parseURL();
+    loadPage(url, false);
+
+    // Add click events to all buttons in the nav-wrapper
+    const navButtons = collapsibleMenuEl.querySelectorAll("a");
+    navButtons.forEach(element => {
+      element.addEventListener(
+        "click",
+        e => {
+          //prevend default action of button
+          e.preventDefault();
+
+          console.log(window.location);
+
+          //check for href of element that was clicked on
+          let pushURL = element.href;
+          const index = pushURL.indexOf("#");
+
+          //if there is no href, the user is trying to go home
+          if (index < 0) pushURL = "home";
+          else pushURL = pushURL.slice(index + 1);
+
+          // Load the page and add it to the history.
+          loadPage(`content/${pushURL}.html`);
+          // update history.
+          window.history.pushState(
+            " ",
+            null,
+            `${window.location.origin}/#${pushURL}`
+          );
+        },
+        true
+      );
+    });
+
+    window.addEventListener(
+      "popstate",
+      _ => {
+        // TODO(rex): Handle the transition.
+        const url = parseURL();
+        loadPage(url, false);
+      },
+      false
+    );
+  }
+
   // Setup the Mobile Nav Menu toggle.
   menuToggleEl.addEventListener("click", _ => {
     toggleNavMenu(collapsibleMenuEl);
