c5a6f476201a400b9afd0ea1ff48ab6cedf857df
rex <retwedt@gmail.com>
2019-06-09 20:55:44 -0500

    Update prettier settings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-    parser: "babel-eslint",
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": ["eslint:recommended", "plugin:prettier/recommended"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [],
-    "rules": {
-        "prettier/prettier": "error",
-        "no-unused-vars": [
-            "error",
-            {
-                varsIgnorePattern: "^_",
-                argsIgnorePattern: "^_"
-            }
-        ],
-    }
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: [],
+  rules: {
+    "prettier/prettier": "error",
+    "no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_"
+      }
+    ]
+  }
 };
